ede1afde02b786e1245aa9bfc058b6b00686e5f0
Sebastian Peiser <peacecake@web.de>
2018-07-25 16:31:57 +0200

    Basic project setup

---
+++ HoloMu App/Assets/Scripts/UI/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float RotationSpeed = 5.0f;
+
+	private void Update ()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime, this.RotationSpeed);	
+	}
+}
