b7f414aa05c275d52e312a2f0ab2ff12cceeab0a
Sebastian Peiser <peacecake@web.de>
2018-08-11 15:03:02 +0200

    #12: Errors get displayed in a global UI

---
+++ HoloMu App/Assets/Scripts/Error.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HoloMu
+{
+    public class Error
+    {
+        public ErrorType Type { get; private set; }
+        public string Message { get; private set; }
+
+        public Error(ErrorType type, string message)
+        {
+            this.Type = type;
+            this.Message = message;
+        }
+	  
+    }
+}
---
+++ HoloMu App/Assets/Scripts/UI/MainUIManager.cs
@@ -0,0 +1,36 @@
+﻿using HoloMu.Networking;
+using HoloMu.Persistance;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HoloMu.UI
+{
+    public class MainUIManager : MonoBehaviour {
+
+        public ApiConnector ApiConnector;
+        public PhotoCapturer PhotoCapturer;
+
+        public Text ErrorText;
+        public float DisplayTextDuration = 4f;
+
+	    private void Start ()
+        {
+            this.ApiConnector.ErrorOccurred += OnErrorOccured;
+            this.PhotoCapturer.ErrorOccured += OnErrorOccured;
+            this.ErrorText.text = "";
+	    }
+
+        private void OnErrorOccured(object sender, Error error)
+        {
+            StartCoroutine(ShowTextForSeconds(ErrorText, error.Message, DisplayTextDuration));
+        }
+
+        private IEnumerator ShowTextForSeconds(Text textField, string text, float time)
+        {
+            textField.text = text;
+            yield return new WaitForSeconds(time);
+            ErrorText.text = "";
+        }
+    }
+}
