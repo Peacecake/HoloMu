cf462fdccbd0f13da4d0e61532f34cfd9296f1b4
Sebastian Peiser <peacecake@web.de>
2018-08-25 17:02:13 +0200

    #21: jsonParser.py added

---
+++ REST Api/flaskapp/jsonParser.py
@@ -0,0 +1,21 @@
+import json
+
+class JsonParser():
+    def __init__(self, jsonFilePath):
+        self.file_path = jsonFilePath
+        self.data = None
+
+    def parse(self):
+        try:
+            with open(self.file_path, "r") as read_file:
+                self.data = json.load(read_file)
+        except:
+            return False
+
+    def get_item_by_id(self, id):
+        if self.data is None:
+            return ""
+        for exhibit in self.data["exhibits"]:
+            if exhibit["id"] == int(id):
+                return str(exhibit)
+        return ""
