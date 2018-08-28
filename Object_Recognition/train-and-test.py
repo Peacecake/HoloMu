53caa857970da7b8907a22dd0d8728e8c1d7f2d4
Sebastian Peiser <peacecake@web.de>
2018-08-28 14:56:32 +0200

    #20: path for trainings data is not fixed anymore

--- Object_Recognition/train-and-test.py
+++ Object_Recognition/train-and-test.py
@@ -7,7 +7,7 @@ def checkIfPicExists():
     for dirpath, dirnames, files in os.walk('./test_image'):
         if files:
             print(dirpath, 'has files')
-            trainOrTest(files[0])
+            trainOrTest(files[1])
         if not files:
             print(dirpath, 'is empty. No Image in direcotry')
     
@@ -18,7 +18,8 @@ def trainOrTest(testImageName):
         os.system("python label_image.py --graph=" + graph + " --image=test_image/" + testImageName)
     else:
         print "training..."
-        os.system("python train_images.py --bottleneck_dir=tf_files/bottlenecks --how_many_training_steps=500 --model_dir=tf_files/models/ --summaries_dir=tf_files/training_summaries/${ARCHITECTURE} --output_graph=tf_files/retrained_graph.pb --output_labels=tf_files/retrained_labels.txt --image_dir=/mnt/k/Users/Tobias/Documents/HoloMu/Object_Recognition/images")
+        training_path = os.path.join(os.getcwd(), "images")
+        os.system("python train_images.py --bottleneck_dir=tf_files/bottlenecks --how_many_training_steps=500 --model_dir=tf_files/models/ --summaries_dir=tf_files/training_summaries/${ARCHITECTURE} --output_graph=tf_files/retrained_graph.pb --output_labels=tf_files/retrained_labels.txt --image_dir=" + training_path)
 
 
 if __name__=='__main__':
