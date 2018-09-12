d3bfb7d7fcb3b8d595497f8dc2732476011ab850
Unknown <Hauser.Tobias@gmx.net>
2018-09-12 19:39:37 +0200

    #5: Init recommender

--- RESTApi/flaskapp/routes.py
+++ RESTApi/flaskapp/routes.py
@@ -1,16 +1,18 @@
 from flask import Blueprint, request, Response
 from db import get_db
+from db import init_db
 from uploader import Uploader
 from jsonParser import JsonParser
 from . import trainAndTest
 from . import label_image
 import os
+from collections import Counter
 
 bp = Blueprint("routes", __name__)
 
 @bp.route("/")
 def hello_world():
-    # init_db()
+    #init_db()
     db = get_db()
     result = db.execute("SELECT * FROM data;").fetchall()
     print (result)
@@ -45,4 +47,21 @@ def recognize_image():
 
 @bp.route("/recommend")
 def recommend_exhibit():
+    db = get_db()
+    #category = db.execute("SELECT e_category from data;").fetchall()
+    category = db.execute("SELECT e_category, count(*) FROM data GROUP BY e_category;").fetchall()
+    #recommendList = []
+    #resultList = []
+    maxProp = 100
+    defaultProp = 0.5
+    for element in range(len(category)):
+        dictElement = dict(category[element])
+        watchedCount = dictElement["count(*)"]
+        currentRecommendResult = maxProp - (1/(defaultProp * watchedCount) *50) #die 50 ist eine magic number um den Wert deutlicher zu machen, da er aber auf beide Berechnungen angewandt wird darf man es machen(kommutativ oder so?)
+        #recommendList.append(dictElement["e_category"] + ";" +str(currentRecommendResult))
+        #result = searchForHighestValue(recommendList, currentRecommendResult)
+        zwischenergebnis = dictElement["e_category"] + ";" + str(currentRecommendResult) +"%"
+        print zwischenergebnis
+        #resultList.append(zwischenergebnis.split(";")[1])
+
     return "Alle Exponate sind super!"
